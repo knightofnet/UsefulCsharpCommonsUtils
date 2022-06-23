8d7b8995e63ba794f8f5e5041ba5f6134ffc9d8c
Aryx <39461001+knightofnet@users.noreply.github.com>
2022-06-23 20:13:26 +0200

    Update version

--- Assembly.cs
+++ Assembly.cs
@@ -6,6 +6,6 @@
 using System.Reflection;
 
 
-[assembly: AssemblyVersion("1.0.1.21")]
-[assembly: AssemblyFileVersion("1.0.1.21")]
+[assembly: AssemblyVersion("1.0.1.22")]
+[assembly: AssemblyFileVersion("1.0.1.22")]
 
